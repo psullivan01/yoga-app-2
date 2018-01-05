5100958e92fd07e7fef354ecc54de8403c38cf00
Adam Ziegele <adamziegele@Adams-MacBook-Pro.local>
2018-01-04 19:30:38 -0500

    Deleted node_modules

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div></div>
-      <div className="App">
+
+       <div>
         <div className="navbar">
 
           <Link to="/workout">Create New Workout</Link>
           <Link to="/my_workouts">My Workouts</Link>
 
         </div>
-        
+
         <Route path="/my_workouts" component={MyWorkouts}/>
         <Route path="/workout" component={WorkoutScreen}/>
-
       </div>
     );
   }
