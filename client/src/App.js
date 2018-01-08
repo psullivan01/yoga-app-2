2d2b9c81df1d0653d825fb87a22a955735533a70
Adam Ziegele <adamziegele@Adams-MacBook-Pro.local>
2018-01-08 11:41:07 -0500

    Added more HTML

--- client/src/App.js
+++ client/src/App.js
@@ -91,8 +91,8 @@ class App extends Component {
                     <div class="features-icons-icon d-flex">
                       <i class="icon-screen-desktop m-auto text-primary"></i>
                     </div>
-                    <h3>Fully Responsive</h3>
-                    <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
+                    <h3>Good for Health</h3>
+                    <p class="lead mb-0">Find balance, harmony, energy, mental clarity, enhance and improve your mood.</p>
                   </div>
                 </div>
                 <div class="col-lg-4">
@@ -100,8 +100,8 @@ class App extends Component {
                     <div class="features-icons-icon d-flex">
                       <i class="icon-layers m-auto text-primary"></i>
                     </div>
-                    <h3>Bootstrap 4 Ready</h3>
-                    <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4 framework!</p>
+                    <h3>Good for Body</h3>
+                    <p class="lead mb-0">Improve metabolism, strengthen and tone muscles, improve balance and increase flexibility.</p>
                   </div>
                 </div>
                 <div class="col-lg-4">
@@ -109,8 +109,8 @@ class App extends Component {
                     <div class="features-icons-icon d-flex">
                       <i class="icon-check m-auto text-primary"></i>
                     </div>
-                    <h3>Easy to Use</h3>
-                    <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
+                    <h3>Good for Cardio</h3>
+                    <p class="lead mb-0">Yoga helps lower blood pressure, increase blood circulation, and decrease physical pain.</p>
                   </div>
                 </div>
               </div>
