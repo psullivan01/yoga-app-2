805cf5a7142eea5d8941f1fcef326501c46ff0c5
psullivan01 <psullivan@hearstautos.com>
2018-01-11 20:38:50 -0500

    added table generation functionality

--- client/src/WorkoutCard.js
+++ client/src/WorkoutCard.js
@@ -2,31 +2,14 @@ import React, { Component } from 'react';
 
 class WorkoutCard extends Component {
 
-	constructor() {
-		super();
-		this.state = {
-			isPicked: false
-		}
-	}
-	
-	onButtonPress() {
-		if (this.state.isPicked) {
-			this.setState({
-				isPicked: false
-			})
-		} else {
-			this.setState({
-				isPicked: true
-			})
-		}
-	}
-
 	render() {
 
 		var muscleClass = "MuscleCard"
+		var buttonText = "Add"
 
-		if (this.state.isPicked) {
+		if (this.props.isPicked) {
 			muscleClass += " isPicked"
+			buttonText = "Remove"
 		}
 
 		return (
@@ -37,7 +20,7 @@ class WorkoutCard extends Component {
 		        	<div className="card-block">
 		          		<h4 className="card-title">{this.props.muscleTitle}</h4>
 		          		<div className="addButton">
-		          			<button type="button" className="btn btn-success btn-sm" onClick={this.onButtonPress.bind(this)} value={this.props.muscle}>Add</button>
+		          			<button type="button" className="btn btn-success btn-sm" onClick={this.props.pickCard}>{buttonText}</button>
 		          		</div>
 		        	</div>
 		        </div>
--- client/src/WorkoutListItem.js
+++ client/src/WorkoutListItem.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 class WorkoutListItem extends Component {
+
 	render() {
+		
 		return (
 			<tbody>
 				<tr>
-					<td>{this.props.totalPoseTime}</td>
+					<td>{this.props.time}</td>
 					<td>{this.props.muscle}</td>
 					<td>{this.props.pose}</td>
 				</tr>
